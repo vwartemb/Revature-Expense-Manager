f853068da133d287c34eaffcc570ee1bba8389ff
Vanessa Wartemberg <vwartemb@depaul.edu>
2026-06-19 23:29:47 -0500

    Had to fix another error with the import statements in launcher

--- manager-app/src/main/java/com/revature/Main.java
+++ manager-app/src/main/java/com/revature/Main.java
@@ -1,9 +1,5 @@
 package com.revature;
-import com.revature.DAOs.AuthDAO;
-import com.revature.DAOs.EmployeeDAO;
-import com.revature.controllers.AuthController;
-import com.revature.controllers.EmployeeController;
-import com.revature.models.Employee;
+
 import io.javalin.Javalin;
 
 import java.util.ArrayList;
